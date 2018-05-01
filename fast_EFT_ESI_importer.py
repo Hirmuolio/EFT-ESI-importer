85fe6b4511147f4d7bae786172cede8bc2667117
Hirmuolio <hirmuolio@gmail.com>
2018-05-01 14:28:28 +0300

    It is now same code as the normal one except for line 313

--- fast_EFT_ESI_importer.py
+++ fast_EFT_ESI_importer.py
@@ -8,6 +8,65 @@ import base64
 #---------------------------
 #Define functions
 #---------------------------
+
+def check_error(esi_response, job):
+	status_code = esi_response.status_code
+	
+	if status_code != 200:
+		#Error
+		print('Failed to '+job+'. Error',esi_response.status_code,'-', esi_response.json()['error'])
+		error = True
+		global has_errors
+		has_errors = True
+	else:
+		error = False
+		try:
+			#Try to print warning
+			print('Warning',esi_response.headers['warning'])
+		except KeyError:
+			warning = False
+	
+	return error
+	
+	
+#Code for logging in
+def logging_in():
+	login_url = 'https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=http://localhost/oauth-callback&client_id='+client_id+'&scope=esi-skills.read_skills.v1+esi-clones.read_implants.v1'
+
+	webbrowser.open(login_url, new=0, autoraise=True)
+
+	authentication_code = input("Give your authentication code: ")
+	
+	combo = base64.b64encode(bytes( client_id+':'+client_secret, 'utf-8')).decode("utf-8")
+	authentication_url = "https://login.eveonline.com/oauth/token"
+	
+	esi_response = requests.post(authentication_url, headers =  {"Authorization":"Basic "+combo}, data = {"grant_type": "authorization_code", "code": authentication_code} )
+	
+	if not check_error(esi_response, 'exchange authorization code for tokens'):
+		response = esi_response.json()
+		
+		access_token = response['access_token']
+		refresh_token = response['refresh_token']
+		
+		character_info = get_char_info(access_token)
+		
+		#Check if character info errored
+		if character_info == 'error':
+			return
+			
+		character_info["refresh_token"] = refresh_token
+
+		config['characters'].append(character_info)
+		
+		print('Character: '+character_info['character_name']+' logged in')
+		
+		#Save new config
+		with open('config.txt', 'w') as outfile:
+			json.dump(config, outfile, indent=4)
+	else:
+		return
+	
+	return config
 	
 #Use refresh token to get new access token
 def refresh_auth(refresh_token):
@@ -16,10 +75,11 @@ def refresh_auth(refresh_token):
 	combo = base64.b64encode(bytes( client_id+':'+client_secret, 'utf-8')).decode("utf-8")
 	esi_response = requests.post(refresh_url, headers =  {"Authorization":"Basic "+combo}, data = {"grant_type": "refresh_token", "refresh_token": refresh_token} )
 	
-	if esi_response.status_code == 200:
+	if not check_error(esi_response, 'refresh access token'):
 		access_token = esi_response.json()['access_token']
 	else:
-		print('Refreshing acces token. Error',esi_response.status_code,'-', esi_response.json()['error'])
+		return 'error'
+
 	return access_token
 	
 #Get info on the character by using the access token
@@ -28,15 +88,66 @@ def get_char_info(access_token):
 	url = 'https://login.eveonline.com/oauth/verify'
 	esi_response = requests.get(url, headers =  {"Authorization":"Bearer "+access_token})
 	
-	if esi_response.status_code == 200:
+	if not check_error(esi_response, 'get character info with access token'):
 		response = esi_response.json()
 		character_id = response['CharacterID']
 		character_name = response['CharacterName']
 		character_info = {"character_id":character_id, "character_name":character_name}
 	else:
-		print('Getting char info. Error',esi_response.status_code,'-', esi_response.json()['error'])
+		return 'error'
+
 	return character_info
 
+def main_menu():
+	#List saved characters
+	if len(config['characters']) == 0:
+		print('\nNo characters saved')
+	else:
+		print('\nSaved characters:')
+		for n in range(0, len(config['characters'])):
+			print(n, config['characters'][n]['character_name'])
+			
+	print('\n[S] Start importing\n[D] Delete characters\n[L] Log in a character\n[R] Reset')
+	user_input = input("[S/D/L/R] ")
+
+	if user_input == 'S' or user_input == 's':
+		import_characters()
+	elif user_input == 'D' or user_input == 'd':
+		delete_characters()
+	elif user_input == 'L' or user_input == 'l':
+		print('Log in a new character.')
+		logging_in()
+	elif user_input == 'R' or user_input == 'r':
+		reset()
+	else:
+		print('[R]un inporting\n[E]dit characters\n[L]og in a character')
+	return
+
+def delete_characters():
+	#Edit characters and return to main menu
+	if len(config['characters']) == 0:
+		print('No characters to delete')
+	else:
+		print('\nSelect the character to delete')
+		for n in range(0, len(config['characters'])):
+			print(n, config['characters'][n]['character_name'])
+			
+		user_input = input("Character number: ")
+		
+		try:
+			user_input = int(user_input)
+			
+			if user_input <= len(config['characters']):
+				del config['characters'][user_input]
+				#Save new config
+				with open('config.txt', 'w') as outfile:
+					json.dump(config, outfile, indent=4)
+			else:
+				print('No character', user_input)
+		except ValueError:
+			print('invalid input', user_input)
+		
+	return
 	
 def import_characters():
 	
@@ -64,6 +175,10 @@ def import_characters():
 			
 			access_token = refresh_auth(refresh_token)
 			
+			#Check if the access token didn't error
+			if access_token == 'error':
+				return
+			
 			#Import skills and put them in the string
 			#import implants and put them to same string
 			#Write the string to file and be done
@@ -74,9 +189,7 @@ def import_characters():
 			url = 'https://esi.tech.ccp.is/v4/characters/'+str(character_id)+'/skills/?datasource=tranquility'
 			
 			esi_response = requests.get(url, headers =  {"Authorization":"Bearer "+access_token})
-			if esi_response.status_code != 200:
-				print(esi_response.status_code,'-', esi_response.json()['error'], '- skills for character: '+character_name)
-			else:
+			if not check_error(esi_response, 'get character skills'):
 				skills = esi_response.json()['skills']
 				
 				for n in range(0, len(skills)):
@@ -99,13 +212,13 @@ def import_characters():
 							print(esi_response.status_code,'-', esi_response.json()['error'], '- skill ID: '+skill_id)
 							
 					output = output + skill_name + '=' + str(skill_level) + '\n'
-			
-			url = 'https://esi.tech.ccp.is/v1/characters/'+str(character_id)+'/implants/?datasource=tranquility'
-			
-			esi_response = requests.get(url, headers =  {"Authorization":"Bearer "+access_token})
-			if esi_response.status_code != 200:
-				print(esi_response.status_code,'-', esi_response.json()['error'], '- implants for character: '+character_name)
 			else:
+				return
+				
+			url = 'https://esi.tech.ccp.is/v1/characters/'+str(character_id)+'/implants/?datasource=tranquility'
+			esi_response = requests.get(url, headers =  {"Authorization":"Bearer "+access_token})
+			
+			if not check_error(esi_response, 'get character implants'):
 				implants = esi_response.json()
 				
 				for n in range(0, len(implants)):
@@ -125,7 +238,9 @@ def import_characters():
 							print(esi_response.status_code,'-', esi_response.json()['error'], '- implant ID: '+implant_id)
 				
 					output = output + 'Implant=' + implant_name + '\n'
-					
+			else:
+				return
+				
 			#save item id names
 			with open("item_id.txt", "w") as outfile:
 				json.dump(item_id, outfile, indent=4)
@@ -143,6 +258,25 @@ def import_characters():
 	
 	return
 	
+def reset():
+	item_id = {}
+	with open('item_id.txt', 'w') as outfile:
+		json.dump(item_id, outfile, indent=4)
+		
+	config = {"client_id":'', "client_secret":'', "characters":[]}
+	with open('config.txt', 'w') as outfile:
+		json.dump(config, outfile, indent=4)
+	
+	#Resets configuration
+	print('\nno client ID or secret found. \nRegister at https://developers.eveonline.com/applications to get them')
+	client_id = input("Give your client ID: ")
+	client_secret = input("Give your client secret: ")
+	config = {"client_id":client_id, "client_secret":client_secret, "characters":[]}
+	with open('config.txt', 'w') as outfile:
+			json.dump(config, outfile, indent=4)
+	item_id = {}
+	with open('item_id.txt', 'w') as outfile:
+		json.dump(item_id, outfile, indent=4)
 	
 	
 #---------------------------
@@ -160,13 +294,24 @@ try:
 		client_id = config['client_id']
 		client_secret = config['client_secret']
 	except KeyError:
-		print('no client ID or secret found. \nUse the full script')
-
+		print('no client ID or secret found. \nRegister at https://developers.eveonline.com/applications to get them')
+		client_id = input("Give your client ID: ")
+		client_secret = input("Give your client secret: ")
+		config = {"client_id":client_id, "client_secret":client_secret, "characters":[]}
+		with open('config.txt', 'w') as outfile:
+			json.dump(config, outfile, indent=4)
+		
 except (IOError, json.decoder.JSONDecodeError):
-	print('no client ID or secret found. \nUse the full script')
+	print('no client ID or secret found. \nRegister at https://developers.eveonline.com/applications to get them')
+	client_id = input("Give your client ID: ")
+	client_secret = input("Give your client secret: ")
+	config = {"client_id":client_id, "client_secret":client_secret, "characters":[]}
+	with open('config.txt', 'w') as outfile:
+		json.dump(config, outfile, indent=4)
 
-run = True
-while run:
-	import_characters()
+
+import_characters()
 
 print('Imprt completed')
+if has_errors == True:
+	input("There were errors. Press enter to exit...")

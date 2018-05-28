d3f0977e7653f6ea6a6223db942aaff33fbefc1a
Hirmuolio <hirmuolio@gmail.com>
2018-05-28 12:02:24 +0300

    Imports esi calls from the other file

--- EFT_ESI_importer.py
+++ EFT_ESI_importer.py
@@ -1,102 +1,36 @@
 #!/usr/bin/env python3
 
-import requests
 import json
-import webbrowser
-import base64
+
+import esi_calling
 
 #---------------------------
 #Define functions
 #---------------------------
 
-def check_error(esi_response, job):
-	status_code = esi_response.status_code
-	
-	if status_code != 200:
-		#Error
-		print('Failed to '+job+'. Error',esi_response.status_code,'-', esi_response.json()['error'])
-		error = True
-		global has_errors
-		has_errors = True
-	else:
-		error = False
-		try:
-			#Try to print warning
-			print('Warning',esi_response.headers['warning'])
-		except KeyError:
-			warning = False
-	
-	return error
-	
-	
-#Code for logging in
+
+
 def logging_in():
-	login_url = 'https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=http://localhost/oauth-callback&client_id='+client_id+'&scope=esi-skills.read_skills.v1+esi-clones.read_implants.v1'
-
-	webbrowser.open(login_url, new=0, autoraise=True)
-
-	authentication_code = input("Give your authentication code: ")
+	#Log in character and save its info
+	scopes = 'esi-skills.read_skills.v1+esi-clones.read_implants.v1'
+	client_id = config['client_id']
+	client_secret = config['client_secret']
 	
-	combo = base64.b64encode(bytes( client_id+':'+client_secret, 'utf-8')).decode("utf-8")
-	authentication_url = "https://login.eveonline.com/oauth/token"
+	tokens = esi_calling.logging_in(scopes, client_id, client_secret)
+	token_info = esi_calling.get_token_info(tokens)
 	
-	esi_response = requests.post(authentication_url, headers =  {"Authorization":"Basic "+combo}, data = {"grant_type": "authorization_code", "code": authentication_code} )
 	
-	if not check_error(esi_response, 'exchange authorization code for tokens'):
-		response = esi_response.json()
-		
-		access_token = response['access_token']
-		refresh_token = response['refresh_token']
-		
-		character_info = get_char_info(access_token)
-		
-		#Check if character info errored
-		if character_info == 'error':
-			return
-			
-		character_info["refresh_token"] = refresh_token
-
-		config['characters'].append(character_info)
-		
-		print('Character: '+character_info['character_name']+' logged in')
-		
-		#Save new config
-		with open('config.txt', 'w') as outfile:
-			json.dump(config, outfile, indent=4)
-	else:
-		return
+	config['characters'][token_info['character_name']] = {}
+	config['characters'][token_info['character_name']]['tokens'] = tokens
+	config['characters'][token_info['character_name']]['character_id'] = token_info['character_id']
+	
+	with open('config.json', 'w') as outfile:
+		json.dump(config, outfile, indent=4)
+	
+	print('Character: '+ token_info['character_name'] +' logged in and saved')
 	
 	return config
 	
-#Use refresh token to get new access token
-def refresh_auth(refresh_token):
-	refresh_url = 'https://login.eveonline.com/oauth/token'
-	
-	combo = base64.b64encode(bytes( client_id+':'+client_secret, 'utf-8')).decode("utf-8")
-	esi_response = requests.post(refresh_url, headers =  {"Authorization":"Basic "+combo}, data = {"grant_type": "refresh_token", "refresh_token": refresh_token} )
-	
-	if not check_error(esi_response, 'refresh access token'):
-		access_token = esi_response.json()['access_token']
-	else:
-		return 'error'
-
-	return access_token
-	
-#Get info on the character by using the access token
-def get_char_info(access_token):
-	
-	url = 'https://login.eveonline.com/oauth/verify'
-	esi_response = requests.get(url, headers =  {"Authorization":"Bearer "+access_token})
-	
-	if not check_error(esi_response, 'get character info with access token'):
-		response = esi_response.json()
-		character_id = response['CharacterID']
-		character_name = response['CharacterName']
-		character_info = {"character_id":character_id, "character_name":character_name}
-	else:
-		return 'error'
-
-	return character_info
 
 def main_menu():
 	#List saved characters
@@ -104,8 +38,8 @@ def main_menu():
 		print('\nNo characters saved')
 	else:
 		print('\nSaved characters:')
-		for n in range(0, len(config['characters'])):
-			print(n, config['characters'][n]['character_name'])
+		for name in config['characters']:
+			print(name)
 			
 	print('\n[S] Start importing\n[D] Delete characters\n[L] Log in a character\n[R] Reset')
 	user_input = input("[S/D/L/R] ")
@@ -129,18 +63,22 @@ def delete_characters():
 		print('No characters to delete')
 	else:
 		print('\nSelect the character to delete')
-		for n in range(0, len(config['characters'])):
-			print(n, config['characters'][n]['character_name'])
+		n = 0
+		name_id = {}
+		for name in config['characters']:
+			print('['+str(n)+']', name)
+			name_id[str(n)] = name
+			n = n+1
 			
 		user_input = input("Character number: ")
 		
 		try:
 			user_input = int(user_input)
 			
-			if user_input <= len(config['characters']):
-				del config['characters'][user_input]
+			if user_input <= len(name_id):
+				del config['characters'][name_id[str(user_input)]]
 				#Save new config
-				with open('config.txt', 'w') as outfile:
+				with open('config.json', 'w') as outfile:
 					json.dump(config, outfile, indent=4)
 			else:
 				print('No character', user_input)
@@ -157,27 +95,24 @@ def import_characters():
 		print('importing characters')
 		#Load saved skill names and implant names
 		try:
-			item_id = json.load(open('item_id.txt'))
+			item_id = json.load(open('item_id.json'))
 		except (IOError, json.decoder.JSONDecodeError):
 			#Some error. Just start the id list from scratch
 			item_id = {}
-			with open('item_id.txt', 'w') as outfile:
+			with open('item_id.json', 'w') as outfile:
 				json.dump(item_id, outfile, indent=4)
 		
 		#Improt characters one by one
-		for n in range(0, len(config['characters'])):
+		for name in config['characters']:
 			
-			character_name = config['characters'][n]['character_name']
-			character_id = config['characters'][n]['character_id']
-			refresh_token = config['characters'][n]['refresh_token']
+			tokens = config['characters'][name]['tokens']
+	
+			tokens = esi_calling.check_tokens(tokens, client_id = config['client_id'], client_secret = config['client_secret'])
 			
-			print('importing '+character_name+'...')
+			character_id = config['characters'][name]['character_id']
 			
-			access_token = refresh_auth(refresh_token)
-			
-			#Check if the access token didn't error
-			if access_token == 'error':
-				return
+			print('\nimporting '+name+'...')
+
 			
 			#Import skills and put them in the string
 			#import implants and put them to same string
@@ -185,68 +120,58 @@ def import_characters():
 			
 			output = ''
 			
-			#Import skills first
-			url = 'https://esi.tech.ccp.is/v4/characters/'+str(character_id)+'/skills/?datasource=tranquility'
+			#Import skills first			
+			esi_response = esi_calling.call_esi(scope = '/v4/characters/{par}/skills', url_parameter=character_id, tokens = tokens, calltype='get', job = 'import character skills')
+
+
+			skills = esi_response.json()['skills']
 			
-			esi_response = requests.get(url, headers =  {"Authorization":"Bearer "+access_token})
-			if not check_error(esi_response, 'get character skills'):
-				skills = esi_response.json()['skills']
+			for n in range(0, len(skills)):
+				skill_id = skills[n]['skill_id']
+				skill_level = skills[n]['active_skill_level']
 				
-				for n in range(0, len(skills)):
-					skill_id = skills[n]['skill_id']
-					skill_level = skills[n]['active_skill_level']
+				#Get item name from the saved list
+				try:
+					skill_name = item_id[str(skill_id)]
+				except KeyError:
+					#Unknown skill. Get name from API
+
+					#print('                \r', end="")#clears the line to stop things from previous loop from staying
+					print(str(n+1)+'/'+str(len(skills)+1) + ' Fetching skill name for ID:', skill_id, end="")
+					esi_response = esi_calling.call_esi(scope = '/v3/universe/types/{par}', url_parameter=skill_id, tokens = tokens, calltype='get', job = 'get implant name')
 					
-					#Get item name from the saved list
-					try:
-						skill_name = item_id[str(skill_id)]
-					except KeyError:
-						#Unknown skill. Get name from API
-						print('Fetching skill name for ID', skill_id,'-', end="")
-						url = "https://esi.tech.ccp.is/v3/universe/types/"+str(skill_id)+"/"
-						esi_response = requests.get(url)
-						if esi_response.status_code == 200:
-							skill_name = esi_response.json()['name']
-							print(' ',skill_name)
-							item_id[skill_id] = skill_name
-						else:
-							print(esi_response.status_code,'-', esi_response.json()['error'], '- skill ID: '+skill_id)
-							
-					output = output + skill_name + '=' + str(skill_level) + '\n'
-			else:
-				return
-				
-			url = 'https://esi.tech.ccp.is/v1/characters/'+str(character_id)+'/implants/?datasource=tranquility'
-			esi_response = requests.get(url, headers =  {"Authorization":"Bearer "+access_token})
+					skill_name = esi_response.json()['name']
+					print(' ', skill_name)
+					item_id[skill_id] = skill_name
+						
+				output = output + skill_name + '=' + str(skill_level) + '\n'
+			print('')
+			esi_response = esi_calling.call_esi(scope = '/v1/characters/{par}/implants', url_parameter=character_id, tokens = tokens, calltype='get', job = 'import character implants')
 			
-			if not check_error(esi_response, 'get character implants'):
-				implants = esi_response.json()
+			implants = esi_response.json()
+			
+			for implant_id in implants:
 				
-				for n in range(0, len(implants)):
-					implant_id = implants[n]
-					
-					try:
-						implant_name = item_id[str(implant_id)]
-					except KeyError:
-						print('Fetching implant name for ID', implant_id,'-', end="")
-						url = "https://esi.tech.ccp.is/v3/universe/types/"+str(implant_id)+"/"
-						esi_response = requests.get(url)
-						if esi_response.status_code == 200:
-							implant_name = esi_response.json()['name']
-							print(' ', implant_name)
-							item_id[implant_id] = implant_name
-						else:
-							print(esi_response.status_code,'-', esi_response.json()['error'], '- implant ID: '+implant_id)
-				
-					output = output + 'Implant=' + implant_name + '\n'
-			else:
-				return
-				
+				try:
+					implant_name = item_id[str(implant_id)]
+				except KeyError:
+					print('                                             \r', end="")#clears the line to stop things from previous loop from staying
+					print('\rFetching implant name for ID:', implant_id, end="")
+					esi_response = esi_calling.call_esi(scope = '/v3/universe/types/{par}', url_parameter=implant_id, tokens = tokens, calltype='get', job = 'get implant name')
+		
+					implant_name = esi_response.json()['name']
+					print(' ', implant_name, end="")
+					item_id[implant_id] = implant_name
+			
+				output = output + 'Implant=' + implant_name + '\n'
+			print('')
+
 			#save item id names
-			with open("item_id.txt", "w") as outfile:
+			with open("item_id.json", "w") as outfile:
 				json.dump(item_id, outfile, indent=4)
 					
 			#Write skills to txt
-			filename = character_name+'.chr'
+			filename = name+'.chr'
 			with open(filename, "w") as text_file:
 				print(output, file=text_file)
 				
@@ -260,11 +185,11 @@ def import_characters():
 	
 def reset():
 	item_id = {}
-	with open('item_id.txt', 'w') as outfile:
+	with open('item_id.json', 'w') as outfile:
 		json.dump(item_id, outfile, indent=4)
 		
 	config = {"client_id":'', "client_secret":'', "characters":[]}
-	with open('config.txt', 'w') as outfile:
+	with open('config.json', 'w') as outfile:
 		json.dump(config, outfile, indent=4)
 	
 	#Resets configuration
@@ -272,10 +197,10 @@ def reset():
 	client_id = input("Give your client ID: ")
 	client_secret = input("Give your client secret: ")
 	config = {"client_id":client_id, "client_secret":client_secret, "characters":[]}
-	with open('config.txt', 'w') as outfile:
+	with open('config.json', 'w') as outfile:
 			json.dump(config, outfile, indent=4)
 	item_id = {}
-	with open('item_id.txt', 'w') as outfile:
+	with open('item_id.json', 'w') as outfile:
 		json.dump(item_id, outfile, indent=4)
 	
 	
@@ -288,7 +213,7 @@ print('\nESI API importer for EFT by Hirmuolio Pine')
 #Check if client ID and client secret are in the onfiguration file.
 #If not ask for them and write them to the file.
 try:
-	config = json.load(open('config.txt'))
+	config = json.load(open('config.json'))
 	
 	try:
 		client_id = config['client_id']
@@ -297,22 +222,20 @@ try:
 		print('no client ID or secret found. \nRegister at https://developers.eveonline.com/applications to get them')
 		client_id = input("Give your client ID: ")
 		client_secret = input("Give your client secret: ")
-		config = {"client_id":client_id, "client_secret":client_secret, "characters":[]}
-		with open('config.txt', 'w') as outfile:
+		config = {"client_id":client_id, "client_secret":client_secret, "characters":{} }
+		with open('config.json', 'w') as outfile:
 			json.dump(config, outfile, indent=4)
 		
 except (IOError, json.decoder.JSONDecodeError):
 	print('no client ID or secret found. \nRegister at https://developers.eveonline.com/applications to get them')
 	client_id = input("Give your client ID: ")
 	client_secret = input("Give your client secret: ")
-	config = {"client_id":client_id, "client_secret":client_secret, "characters":[]}
-	with open('config.txt', 'w') as outfile:
+	config = {"client_id":client_id, "client_secret":client_secret, "characters":{} }
+	with open('config.json', 'w') as outfile:
 		json.dump(config, outfile, indent=4)
 
 run = True
 while run:
 	main_menu()
 
-print('Imprt completed')
-if has_errors == True:
-	input("There were errors. Press enter to exit...")
+print('\nImport completed')
